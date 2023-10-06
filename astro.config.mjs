6aeb5b64e40a376b1a51f6f02dc39002b680f121
Gabriel Gutierrez <ggutierrezortegon@gmail.com>
2023-10-06 10:22:31 -0500

    Configurando Deploy
    - Se ha agregado un archivo yml para configurar el deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [tailwind()],
+  site: 'https://g4bo11.github.io',
+  base: '/Portfolio'
 });
